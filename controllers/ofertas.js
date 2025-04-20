8a243af0f917d027584c0ac8c006f2346e94c38d
nicovidal <nico.vidal.m@gmail.com>
2025-04-20 19:12:31 -0400

    service rut ok

--- controllers/ofertas.js
+++ controllers/ofertas.js
@@ -1,20 +1,55 @@
 const { response } = require("express");
-const Oferta = require("../models/ofertas");
+const { crearOfertaService, obtenerOfertasService } = require("../services/oferta.service");
 
 const crearOferta = async (req, res = response) => {
   try {
-    const oferta = new Oferta(req.body);
-
-    await oferta.save();
+    await crearOfertaService(req.body)
 
     res.status(201).json({
       ok: true,
       msg: "oferta guardado correctamente",
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log('error')
+  }
 };
 
 
+const obtenerOfertas=async(req,res=response)=>{
+  try {
+    
+   const ofertas= await obtenerOfertasService()
+
+    res.json({ofertas})
+
+  } catch (error) {
+    
+  }
+}
+
+const obtenerOfertaRutService=async(req,res=response)=>{
+  
+  const {rut}=req.query
+
+  try {
+    
+    const ofertaRut=await obtenerOfertaRutService({rut})
+
+    if (!ofertaRut) {
+      return res.status(400).json({
+        ok: false,
+        msg: "oferta no existe",
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
 module.exports={
-    crearOferta
+    crearOferta,
+    obtenerOfertas,
+    obtenerOfertaRutService
 }
