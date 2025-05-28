876270fb61fed816c35856c783cbcda8dd956a83
nicovidal <nico.vidal.m@gmail.com>
2025-05-28 19:48:26 -0400

    ruta tabla rangos crear y obtener

--- services/tabla-rangos.service.js
+++ services/tabla-rangos.service.js
@@ -1,19 +1,16 @@
-const TablaRangos = require("../models/tabla-rangos");
+const TablaRango = require("../models/tabla-rangos");
 
 
-const crearTablaRangoService=async(tablaRangosData)=>{
+const crearTablaRangoService = async (tablaRangosData) => {
+  const tablaRango = new TablaRango(tablaRangosData);
+  await tablaRango.save();
+  return tablaRango;
+};
 
-    const tablaRangos=new TablaRangos(req.body)
-
-    return tablaRangos;
-}
-
-const obtenerTablaRangosService=async()=>{
-    const tablaRangos=new TablaRangos.find();
-    return tablaRangos
-}
-
-
+const obtenerTablaRangosService = async () => {
+  const tablaRangos = await TablaRango.find();
+  return tablaRangos;
+};
 module.exports={
     crearTablaRangoService,
     obtenerTablaRangosService
